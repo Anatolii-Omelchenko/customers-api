29373ce4030e4abc06a91928d6ee8cc5c9737854
Anatolii <omelchenko20@gmail.com>
2024-08-02 16:50:46 +0300

    initial commit

---
+++ src/main/java/tech/theraven/customers_api/CustomersApiApplication.java
@@ -0,0 +1,13 @@
+package tech.theraven.customers_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/theraven/customers_api/CustomersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.theraven.customers_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
